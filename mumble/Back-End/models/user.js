79ae7a0ad7e99d2958254d89ea865b71bbe1db0e
Tri-Tacyhon <jrbenny10@gmail.com>
2025-04-02 19:38:44 -0400

    Created Mongo Database, created userSchema, and created server.js. Will implement login, logout, signup functionality next.

---
+++ mumble/Back-End/models/user.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    Username: {
+        type: String,
+        require: true
+    },
+    Password: {
+        type: String,
+        require: true
+    }
+}, { timestamps: true });
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
